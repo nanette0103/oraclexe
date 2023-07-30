ee616f764c93b54a91a8a19b2481880f508fd697
nanette0103 <139376417+nanette0103@users.noreply.github.com>
2023-07-30 17:39:21 +0900

    oracle2

---
+++ ex04-select-quiz.sql
@@ -0,0 +1,31 @@
+--Q1."employees" 테이블에서 모든 직원들의 성(First name), 이름(Last name)을 조회하세요
+
+SELECT first_name, last_name
+FROM employees;
+
+--Q2."jobs" 테이블에서 모든 직무들의 직무 id(job id)와 직무명(job title)을 조회하세요
+SELECT job_id, job_title
+FROM jobs;
+
+--Q3."departments" 테이블에서 모든 부서들의 부서 ID(department id)와 부서명(department name)을 조회하세요
+SELECT department_id, department_name
+FROM departments;
+
+--Q4."locations" 테이블에서 모든 지역들의 지역 ID(location id)와 도시(city)를 조회하세요.
+SELECT location_id, city
+FROM locations;
+
+--Q5."employees" 테이블에서 급여(salary)가 5000이상인 직원들의 이름(first name)과 급여(salary)를 조회하세요.
+SELECT first_name, salary
+FROM employees
+WHERE salary >= 5000;
+
+--Q6."employees" 테이블에서 근무 시작일(hire date)이 2005년 이후인 직원들의 이름(first name)과 근무시작일(hire date)를 조회하세요.
+
+SELECT first_name, hire_date
+FROM employees
+WHERE hire_date > '05/01/01';
+
+SELECT first_name, hire_date
+FROM employees
+WHERE hire_date > TO_DATE('2005-01-01', 'YYYY-MM-DD');
